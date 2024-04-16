8fca3ac3e93b1e85a33143f889a8bbd165f6e32f
JuanDiego1423 <2202050@utrivieramaya.edu.mx>
2024-04-16 13:20:44 -0500

    report 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/view/form_task.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -58,9 +59,12 @@ class _MyHomePageState extends State<MyHomePage> {
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('Hola');
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AddNewTaskScreen()));
         },
-        tooltip: 'Increment',
+        tooltip: 'Agregar nueva tarea',
         child: const Icon(Icons.add),
       ),
     );
